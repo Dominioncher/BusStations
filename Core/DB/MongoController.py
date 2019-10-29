fcfeef82b37829b19457a550496e8e0c4e78403f
Artyom <kushnirartyom@yandex.ru>
2019-10-29 23:49:26 +0500

    Migrate to another mongoDB cluster

--- Core/DB/MongoController.py
+++ Core/DB/MongoController.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 
-connection = MongoClient("mongodb+srv://BusStation:<password>@busstations-fcpis.mongodb.net/test?retryWrites=true&w=majority")
-db = connection['BusStations']
+connection = MongoClient("mongodb+srv://BusStation:BusStation@flamber-mongodb-pjxiq.mongodb.net/test?retryWrites=true&w=majority")
+db = connection['BusStation']
