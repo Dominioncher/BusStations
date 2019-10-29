5444e8a885f20ae5a02522d4436bcf2b4bb89532
Egor <gorich60@gmail.com>
2019-10-29 22:56:12 +0500

    Преобразование результата запросов в словарь

--- data_loader.py
+++ data_loader.py
@@ -1,19 +1,22 @@
+import json
+
 import requests
 
 
 def get_routes():
-    return requests.get('https://api.tgt72.ru/api/v5/route/')
+    return requests.get('https://api.tgt72.ru/api/v5/route/').json()
 
 
 def get_checkpoints():
-    return requests.get('https://api.tgt72.ru/api/v5/checkpoint/')
+    return requests.get('https://api.tgt72.ru/api/v5/checkpoint/').json()
 
 
 def get_road_works():
-    return requests.get('https://api.tgt72.ru/api/v5/roadworks/')
+    return requests.get('https://api.tgt72.ru/api/v5/roadworks/').json()
 
 
+# Если запустить в консоли питона, можно поглядеть на данные
 if __name__ == '__main__':
-    get_road_works()
-    get_checkpoints()
-    get_routes()
+    works = get_road_works()
+    checkpoints = get_checkpoints()
+    routes = get_routes()
