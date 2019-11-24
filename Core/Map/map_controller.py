3a9107a839a97345a72d3d4aa381d2cd4844ef2a
Artyom <kushnirartyom@yandex.ru>
2019-11-24 14:41:09 +0500

    Map Drawing

--- Core/Map/map_controller.py
+++
@@ -1,7 +0,0 @@
-from Core.DataLoaders.DataLoader import get_checkpoints
-from gmplot import gmplot
-
-if __name__ == '__main__':
-    gmap = gmplot.GoogleMapPlotter(30.3164945,
-                                   78.03219179999999, 13)
-    gmap.draw('test.html')
--- Core/main.py
+++
@@ -1,8 +0,0 @@
-import Core.Graph.Graph as Bus
-
-graph = Bus.BusGraph()
-graph.load_data()
-graph.modify_checkpoints([1])
-graph.add_checkpoint("ИМИКН", 57.158820, 65.522632)
-graph.remove_checkpoint(1)
-graph.optimize()
