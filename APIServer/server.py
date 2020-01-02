91e110759bf22633ad6e6c4843913bbca8bd55e1
Artyom <kushnirartyom@yandex.ru>
2020-01-02 17:21:16 +0500

    Добавление остановок на карте Остановки с графа Режимы редактирования добавления удаления

--- APIServer/server.py
+++ APIServer/server.py
@@ -1,22 +1,29 @@
 import flask
-from Core.DB.MongoController import db
+from flask import request
 from bson.json_util import dumps
+import Core.Graph.Graph as Bus
 
 app = flask.Flask(__name__)
+
+graph = Bus.BusGraph()
+graph.load_data()
 
 
 @app.route('/checkpoints', methods=['GET'])
 def get_checkpoints():
-    query = {}
-    query["routes_ids"] = 959  # 30ый маршрут
-    checkpoints = list(db['checkpoints'].find(query))
-
-    resp = flask.Response(status=200)
-    resp.data = dumps(checkpoints)
-    resp.content_type = "application/json"
-    return resp
+    checkpoints = []
+    for node in graph.graph.nodes:
+        checkpoints.append(graph.graph.nodes[node]['checkpoint'])
+    return dumps(checkpoints)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def get_map_html():
     return flask.render_template('map.html')
+
+
+@app.route('/addCheckpoint', methods=['GET'])
+def add_checkpoint():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    return graph.add_checkpoint('Новая остановка', lat, lon)
--- APIServer/static/polylineEditor.js
+++ APIServer/static/polylineEditor.js
@@ -1,13 +1,25 @@
 ymaps.ready(init);
 
+var myMap;
+var objectManager;
+
 function init() {
-    // Создаем карту.
-    var myMap = new ymaps.Map("map", {
+    createMap();
+    clickEvent();
+    loadStations();
+    addButtons();
+}
+
+
+// Создаем карту.
+function createMap() {
+    myMap = new ymaps.Map("map", {
         center: [57.151293, 65.537817],
         zoom: 15
     }, {
         searchControlProvider: 'yandex#search'
     });
+
     objectManager = new ymaps.ObjectManager({
         // Чтобы метки начали кластеризоваться, выставляем опцию.
         clusterize: true,
@@ -21,43 +33,113 @@ function init() {
     objectManager.objects.options.set('preset', 'islands#blueDotIcon');
     objectManager.clusters.options.set('preset', 'islands#blueClusterIcons');
     myMap.geoObjects.add(objectManager);
+}
 
-    // Узнаем по клику координаты (должны узнавать по идее)
+// Узнаем по клику координаты
+function clickEvent() {
     myMap.events.add('click', function (e) {
         var coords = e.get('coords');
         console.log('lat=',coords[0].toPrecision(7),',lon=', coords[1].toPrecision(7),',name=1');
+        if (addMode){
+            addNewStation(coords);
+            console.log('Добавили точку');
+            loadStations()
+        }
     });
 
-    // Запрос к нашей api для получения остановок
-    const Http = new XMLHttpRequest();
-    const url = '/checkpoints';
+}
+
+// Ручное добавление новой остановки
+function addNewStation(coords) {
+    var Http = new XMLHttpRequest();
+    Http.open('GET', `/addCheckpoint?lat=${coords[0]}&lon=${coords[1]}`);
+    Http.send('');
+}
+
+// Запрос к нашей api для получения остановок
+function loadStations() {
+    var Http = new XMLHttpRequest();
     Http.onreadystatechange = (e) => {
-        if (Http.status == 200) {
-            values = JSON.parse(Http.responseText);
-            features = []
-            values.forEach(x => features.push(AddCheckpoint(x)))
+        if (Http.status === 200) {
+            var values = JSON.parse(Http.responseText);
+            var features = [];
+            values.forEach(x => features.push(AddCheckpoint(x)));
             values = {
                 type: 'FeatureCollection',
                 features: features
-            }
-            objectManager.add(values)
+            };
+            objectManager.removeAll();
+            objectManager.add(values);
+            //addBusLine(features)
         }
-    }
-
-    function AddCheckpoint(value) {
-        return {
-            type: "Feature",
-            id: value.id,
-            geometry: {
-                type: "Point",
-                coordinates: [value.projected_lat, value.projected_lon]
-            },
-            properties: {
-                balloonContentHeader: value.name + '<br>' + value.description
-            }
-        }
-    }
-
-    Http.open('GET', url);
+    };
+    Http.open('GET', '/checkpoints');
     Http.send('');
+}
+
+// Добавить метку остановки на карту
+function AddCheckpoint(value) {
+    return {
+        type: "Feature",
+        id: value.id,
+        geometry: {
+            type: "Point",
+            coordinates: [value.lat, value.lon]
+        },
+        properties: {
+            balloonContentHeader: value.name + '<br>'
+                + value.description + '<br>' +
+                ''
+        }
+    }
+}
+
+// Соединяем остановки
+function addBusLine(features) {
+    var dots = [];
+    features.forEach(x => dots.push(x.geometry.coordinates));
+    var myPolyline = new ymaps.Polyline(dots, {
+        // Описываем свойства геообъекта.
+        // Содержимое балуна.
+        balloonContent: "Ломаная линия"
+    }, {
+        // Задаем опции геообъекта.
+        // Отключаем кнопку закрытия балуна.
+        balloonCloseButton: false,
+        // Цвет линии.
+        strokeColor: "#000000",
+        // Ширина линии.
+        strokeWidth: 4,
+        // Коэффициент прозрачности.
+        strokeOpacity: 0.5
+    });
+    myMap.geoObjects.add(myPolyline)
+}
+
+// Текуший режим карты
+var editMode = false;
+var addMode = false;
+var removeMode = false;
+
+// Добавление кнопок редактировать, добавить, удалить
+function addButtons() {
+    var button1 = new ymaps.control.Button("Редактировать");
+    button1.events.add(['press'], function (sender) {
+       editMode= addMode = removeMode = false;
+       editMode = !sender.originalEvent.target.isSelected();
+    });
+    var button2 = new ymaps.control.Button("Добавить");
+    button2.events.add(['press'], function (sender) {
+        editMode= addMode = removeMode = false;
+        addMode = !sender.originalEvent.target.isSelected();
+    });
+    var button3 = new ymaps.control.Button("Удалить");
+    button3.events.add(['press'], function (sender) {
+        editMode= addMode = removeMode = false;
+        removeMode = !sender.originalEvent.target.isSelected();
+    });
+
+    myMap.controls.add(button1, {float: 'right'});
+    myMap.controls.add(button2, {float: 'right'});
+    myMap.controls.add(button3, {float: 'right'});
 }
